7b5a6724d0bac5d98888a7fce855ddbbf980577d
Ogaga <iyaraogaga69@gmail.com>
2022-05-10 11:28:45 +0100

    Modify webpack configurations

--- dist/bundle54d450fc9cfbf0cb7192.js
+++
@@ -1,9 +0,0 @@
-/******/ (() => { // webpackBootstrap
-var __webpack_exports__ = {};
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-console.log(123);
-/******/ })()
-;
-//# sourceMappingURL=bundle54d450fc9cfbf0cb7192.js.map
--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,36 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        bundle: path.resolve(__dirname, 'src/index.js')
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: '[name][contenthash].js',
-        clean: true,
-    },
-    devtool: 'source-map',
-    devServer: {
-        static:{
-            directory: path.resolve(__dirname, 'dist')
-        },
-        port: 3000,
-        open: true,
-        hot: true,
-        compress: true,
-        historyApiFallback: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-        ],
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Webpack App',
-            filename: 'index.html',
-            template: 'src/template.html'
-         }),
+  mode: 'development',
+
+  entry: './src/index.js',
+
+  devtool: 'inline-source-map',
+
+  devServer: {
+    static: './dist',
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin ({
+      template: './src/index.html'
+    }),
+  ],
+
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+//   optimization: {
+//     runtimeChunk: 'single',
+//   }, 
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-}
+  },
+};
