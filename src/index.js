e3ebeab9583bcaf91b118f0e91848b7104cb01d2
Ogaga <iyaraogaga69@gmail.com>
2022-05-10 12:46:24 +0100

    Add page title

--- src/index.js
+++ src/index.js
@@ -1,2 +1,15 @@
 import './style.css';
 
+
+const body = document.querySelector('body');
+const header = document.createElement('header');
+const title = document.createElement('h1');
+title.innerHTML= "Leaderboard";
+header.appendChild(title);
+body.appendChild(header)
+
+
+
+let a = 6;
+let b = 3;
+console.log(a+b)
