d66572d737d9c838767bd84e594ba2aaf854ca88
Ogaga <iyaraogaga69@gmail.com>
2022-05-10 17:55:55 +0100

    Fix linters error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import './style.css';
-import { refreshTitle } from './refreshscore.js';
-import { displayScore } from './refreshscore.js';
-import { addScore } from './styles/addscore.js';
+import { refreshTitle, displayScore } from './refreshscore.js';
+import { addScore } from './addscore.js';
 
 const body = document.querySelector('body');
 const header = document.createElement('header');
 const title = document.createElement('h1');
-title.innerHTML= "Leaderboard";
+title.innerHTML = 'Leaderboard';
 header.appendChild(title);
-body.appendChild(header)
+body.appendChild(header);
 const main = document.createElement('main');
 body.appendChild(main);
 const divD = document.createElement('div');
@@ -19,7 +18,7 @@ const addScoreSection = document.createElement('section');
 addScoreSection.setAttribute('id', 'add-section');
 divD.appendChild(refreshSection);
 divD.appendChild(addScoreSection);
-main.appendChild(divD)
+main.appendChild(divD);
 refreshSection.appendChild(refreshTitle());
 refreshSection.appendChild(displayScore());
 addScoreSection.appendChild(addScore());
--- src/refreshscore.js
+++ src/refreshscore.js
@@ -1,44 +1,44 @@
-function refreshTitle(){
-    const divA = document.createElement('div');
-    divA.setAttribute('id', 'ref-div');
-    const reftitle = document.createElement('h2');
-    reftitle.innerHTML = "Refresh Score";
-    const refButton = document.createElement('button');
-    refButton.setAttribute('id', 'ref-button');
-    refButton.innerHTML = "Refresh"
-    divA.appendChild(reftitle);
-    divA.appendChild(refButton);
-    return divA;
+function refreshTitle() {
+  const divA = document.createElement('div');
+  divA.setAttribute('id', 'ref-div');
+  const reftitle = document.createElement('h2');
+  reftitle.innerHTML = 'Refresh Score';
+  const refButton = document.createElement('button');
+  refButton.setAttribute('id', 'ref-button');
+  refButton.innerHTML = 'Refresh';
+  divA.appendChild(reftitle);
+  divA.appendChild(refButton);
+  return divA;
 }
 
-function displayScore(){
-    const divB = document.createElement('div');
-    divB.setAttribute('id', 'display');
-    const para1 = document.createElement('p');
-    para1.setAttribute('class', 'white');
-    para1.innerHTML = "Julius: 70";
-    const para2 = document.createElement('p');
-    para2.setAttribute('class', 'black');
-    para2.innerHTML = "Tega: 88";
-    const para3 = document.createElement('p');
-    para3.setAttribute('class', 'white');
-    para3.innerHTML = "Vincent: 73";
-    const para4 = document.createElement('p');
-    para4.setAttribute('class', 'black');
-    para4.innerHTML = "Bush: 79";
-    const para5 = document.createElement('p');
-    para5.setAttribute('class', 'white');
-    para5.innerHTML = "Leonard: 97";
-    const para6 = document.createElement('p');
-    para6.setAttribute('class', 'black');
-    para6.innerHTML = "Damian: 79";
-    divB.appendChild(para1);
-    divB.appendChild(para2);
-    divB.appendChild(para3);
-    divB.appendChild(para4);
-    divB.appendChild(para5);
-    divB.appendChild(para6);
-    return divB
+function displayScore() {
+  const divB = document.createElement('div');
+  divB.setAttribute('id', 'display');
+  const para1 = document.createElement('p');
+  para1.setAttribute('class', 'white');
+  para1.innerHTML = 'Julius: 70';
+  const para2 = document.createElement('p');
+  para2.setAttribute('class', 'black');
+  para2.innerHTML = 'Tega: 88';
+  const para3 = document.createElement('p');
+  para3.setAttribute('class', 'white');
+  para3.innerHTML = 'Vincent: 73';
+  const para4 = document.createElement('p');
+  para4.setAttribute('class', 'black');
+  para4.innerHTML = 'Bush: 79';
+  const para5 = document.createElement('p');
+  para5.setAttribute('class', 'white');
+  para5.innerHTML = 'Leonard: 97';
+  const para6 = document.createElement('p');
+  para6.setAttribute('class', 'black');
+  para6.innerHTML = 'Damian: 79';
+  divB.appendChild(para1);
+  divB.appendChild(para2);
+  divB.appendChild(para3);
+  divB.appendChild(para4);
+  divB.appendChild(para5);
+  divB.appendChild(para6);
+  return divB;
 }
 
-export {refreshTitle, displayScore}
+export { refreshTitle, displayScore };
