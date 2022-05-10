cf4bba0e87cb4154528823c908e2f6935ebfd761
Ogaga <iyaraogaga69@gmail.com>
2022-05-10 11:18:45 +0100

    Add webpack configurations

---
+++ dist/bundle54d450fc9cfbf0cb7192.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log(123);
+/******/ })()
+;
+//# sourceMappingURL=bundle54d450fc9cfbf0cb7192.js.map
